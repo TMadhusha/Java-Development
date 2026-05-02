427126e85ac5b27d17e44d93f80f9a7f176fe528
TMadhusha <122914674+TMadhusha@users.noreply.github.com>
2026-05-02 16:21:50 +0530

    commit

--- Java Develoment/src/Task_01/CalculatorApplication.java
+++ Java Develoment/src/Task_01/CalculatorApplication.java
@@ -3,53 +3,41 @@ import java.util.Scanner;
 
 public class CalculatorApplication {
     public static void add(int a, int b){
-        try{
-            System.out.println(a+b);
-        }catch (Exception e){
-            System.out.println("Error" + e);
-        }
+            System.out.println("Answer: " + (a+b));
     }
     public static void sub(int a, int b){
-        try{
-            System.out.println(a-b);
-        }catch (Exception e){
-            System.out.println("Error");
-        }
+        System.out.println("Answer: " + (a-b));
     }
 
     public static void mul(int a, int b){
-        try{
-            System.out.println(a*b);
-        }catch (Exception e){
-            System.out.println("Error");
-        }
+        System.out.println("Answer: " + a*b);
     }
 
     public static void div(int a, int b){
-        try{
-            System.out.println(a/b);
-        }catch (Exception e){
-            if(a/b)
+        if(b==0){
+            System.out.println("Can't divide by zero");
+        }else{
+            System.out.println("Answer: " + a/b);
         }
+
     }
     public static void main(String[] args) {
         //user inputs numbers
-        Scanner input1 = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter first number: ");
-        int i=input1.nextInt();
-        Scanner input2 = new Scanner(System.in);
+        int i=input.nextInt();
         System.out.println("Enter second number: ");
-        int j = input2.nextInt();
+        int j = input.nextInt();
 
         //choose method
         System.out.println("Please choose and type the relevant number from below");
         System.out.println("1. Add");
-        System.out.println("2.sub");
+        System.out.println("2. sub");
         System.out.println("3. Mul");
         System.out.println("4. Div");
-        Scanner input3 = new Scanner(System.in);
+
         System.out.println("Enter the number here: ");
-        int k = input3.nextInt();
+        int k = input.nextInt();
 
         switch (k){
             case 1:
@@ -60,8 +48,10 @@ public class CalculatorApplication {
                 break;
             case 3:
                 mul(i,j);
+                break;
             case 4:
                 div(i,j);
+                break;
             default:
                 System.out.println("Invalid choose");
         }
